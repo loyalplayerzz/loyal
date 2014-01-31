117784c87c15d8d62a7e5fa4acd4a4479aafb204
Ashok B <Ashok.Balasubramanian@kaiser.org>
2014-01-31 12:40:27 -0800

    Updated ID values

--- loyalwebservice/src/main/java/com/loyal/service/pojo/LoyalGifts.java
+++ loyalwebservice/src/main/java/com/loyal/service/pojo/LoyalGifts.java
@@ -43,6 +43,16 @@ public class LoyalGifts {
 	public void setImageURL(String imageURL) {
 		this.imageURL = imageURL;
 	}
+	
+	private String id;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
 
 	private String name;
 	
